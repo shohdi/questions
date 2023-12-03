5b9fa50c685128285b5f5e8e0b4548ec27d7754e
Shohdy ELSheemy <shohdi@gmail.com>
2023-12-03 16:27:13 +0300

    add db context to program

--- questions/Program.cs
+++ questions/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using questions.Data;
+
 namespace questions
 {
 	public class Program
@@ -9,7 +12,15 @@ namespace questions
 			// Add services to the container.
 			builder.Services.AddRazorPages();
 
-			var app = builder.Build();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+			 options.UseSqlServer(
+			 builder.Configuration.GetConnectionString("DefaultConnection")
+			 )
+			);
+
+
+            var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
