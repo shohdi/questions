c20cd88406847dd58f7ae317fbcef52b8e74faa4
Shohdy ELSheemy <shohdi@gmail.com>
2023-12-04 22:51:52 +0300

    create first controller

---
+++ questions/Pages/ExamRepoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace questions.Pages
+{
+	
+	public class ExamRepoController : Controller
+	{
+        
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
