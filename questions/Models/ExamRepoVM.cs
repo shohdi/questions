f20e914d3b802aad18c9873db9c21efaede9776c
Shohdy ELSheemy <shohdi@gmail.com>
2023-12-04 23:34:53 +0300

    first controller

---
+++ questions/Models/ExamRepoVM.cs
@@ -0,0 +1,14 @@
+﻿using questions.Data.Models;
+
+namespace questions.Models
+{
+    public class ExamRepoVM
+    {
+        public ExamRepoVM()
+        {
+            this.AllExams = new List<EXAM_REPOSITORY>();
+        }
+        public List<EXAM_REPOSITORY> AllExams { get; set; }
+
+    }
+}
--- questions/Pages/ExamRepoController.cs
+++ questions/Pages/ExamRepoController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using questions.Data;
+using questions.Models;
 
 namespace questions.Pages
 {
 	
 	public class ExamRepoController : Controller
 	{
+        private readonly ILogger<ExamRepoController> logger;
+		private readonly ApplicationDbContext context;
+        public ExamRepoController(ILogger<ExamRepoController> _logger
+			,ApplicationDbContext _context) {
+			this.logger = _logger;
+			this.context = _context;
+		}
         
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
 		{
-			return View();
+			var myModel = new ExamRepoVM();
+			//read exams
+			myModel.AllExams =  await this.context.ExamRepositories.ToListAsync();
+
+			return View(myModel);
 		}
 	}
 }
