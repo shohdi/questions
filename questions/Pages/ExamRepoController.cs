d9038767dcbefdf74f2336e9a074b2fd1ce94549
Shohdy ELSheemy <shohdi@gmail.com>
2023-12-07 23:26:50 +0300

    finish backend for adding

--- questions/Pages/ExamRepoController.cs
+++ questions/Pages/ExamRepoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using questions.Data;
+using questions.Data.Models;
 using questions.Models;
 using System.Runtime.CompilerServices;
 
@@ -30,8 +31,18 @@ namespace questions.Pages
         [HttpGet]
         public async Task<IActionResult> Repository(long? RepoId = null)
 		{
+			ExamRepoPostVM myModel = new ExamRepoPostVM();
+			if(RepoId != null)
+			{
+                var oldExam = await this.context.ExamRepositories.Where(w => w.ID == RepoId).FirstOrDefaultAsync();
+				if (oldExam != null)
+				{
+					myModel.Name = oldExam.NAME;
+					ViewBag.RepoId = RepoId;
+				}
+            }
 
-			return View();
+            return View(myModel);
 		}
 
         [HttpPost]
@@ -39,8 +50,57 @@ namespace questions.Pages
         {
 			if(ModelState.IsValid)
 			{
-				//var oldExam = this.Repository
-			}
+
+				if (RepoId == null)
+				{
+					//new model
+					var oldExam = await this.context.ExamRepositories.Where(w => w.NAME == myModel.Name).FirstOrDefaultAsync();
+					if (oldExam != null)
+					{
+						ModelState.AddModelError("", "Exam already found before!");
+
+					}
+					if (ModelState.IsValid)
+					{
+						EXAM_REPOSITORY dbModel = new EXAM_REPOSITORY();
+						dbModel.NAME = myModel.Name;
+						await this.context.AddAsync(dbModel);
+						TempData["message"] = "Added Successfully!";
+						TempData["class"] = "alert alert-success";
+						return RedirectToAction("Index");
+
+					}
+					else
+					{
+						return View(myModel);
+					}
+				}
+				else
+				{
+                    //edit model
+                    var oldExam = await this.context.ExamRepositories.Where(w => w.ID ==RepoId).FirstOrDefaultAsync();
+                    if (oldExam == null)
+                    {
+                        ModelState.AddModelError("", "Exam not found!");
+
+                    }
+                    if (ModelState.IsValid)
+                    {
+						oldExam.NAME = myModel.Name;
+						await this.context.SaveChangesAsync();
+                        TempData["message"] = "Saved Successfully!";
+                        TempData["class"] = "alert alert-success";
+                        return RedirectToAction("Index");
+
+                    }
+                    else
+                    {
+                        return View(myModel);
+                    }
+
+                }
+
+            }
 			else
 			{
                 return View(myModel);
