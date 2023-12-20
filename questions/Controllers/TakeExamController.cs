a1ca01a8bb6768e66ec01da3e08b056603ff6bcb
Shohdy ELSheemy <shohdi@gmail.com>
2023-12-20 21:07:33 +0300

    created new page for take exam

---
+++ questions/Controllers/TakeExamController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using questions.Data;
+using questions.Models;
+
+namespace questions.Controllers
+{
+    public class TakeExamController : Controller
+    {
+        private readonly ILogger<QuestionsController> logger;
+        private readonly ApplicationDbContext context;
+        
+        public TakeExamController(ILogger<QuestionsController> _logger
+            , ApplicationDbContext _context
+            )
+        {
+            logger = _logger;
+            context = _context;
+           
+        }
+        public async Task<IActionResult> Index()
+        {
+            TakeExamVM myModel = new TakeExamVM();
+            myModel.AvailableExams = await this.context.ExamRepositories.ToListAsync();
+            return View(myModel);
+        }
+    }
+}
---
+++ questions/Models/TakeExamVM.cs
@@ -0,0 +1,13 @@
+﻿using questions.Data.Models;
+
+namespace questions.Models
+{
+    public class TakeExamVM
+    {
+        public TakeExamVM()
+        {
+            AvailableExams = new List<EXAM_REPOSITORY>();
+        }
+        public List<EXAM_REPOSITORY> AvailableExams { get; set; }
+    }
+}
